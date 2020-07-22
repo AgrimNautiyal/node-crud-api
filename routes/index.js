be22772063689ab7480b1f5dd370174f89ff3dce
unknown <agrimnautiyal1999@gmail.com>
2020-07-22 23:54:41 +0530

    Connected with MongoDB - Atlas

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,37 @@
 var router = require('express').Router();
 var request = require('request');
+const {MongoClient} = require('mongodb');
 
+require('dotenv').config();
+var uname = process.env.uname;
+var password = process.env.password;
+var db_name = process.env.db_name;
 
-router.get('/', function(req, res){
+const uri = `mongodb+srv://${uname}:${password}@cluster0.ntbt5.mongodb.net/${db_name}?retryWrites=true&w=majority`;
+
+router.get('/', async function(req, res){
   console.log("Home route activated");
-    res.json({message : 'response'});
+  const client = new MongoClient(uri ,{ useNewUrlParser: true, useUnifiedTopology: true });
+  try {
+       // Connect to the MongoDB cluster
+       await client.connect();
+       console.log('connected Successfully');
+       // Make the appropriate DB calls
+       await  listDatabases(client);
+
+   } catch (e) {
+       console.error(e);
+   } finally {
+       await client.close();
+   }
+   res.json({message : 'Successfully connected to mongo'});
 });
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
 
 //to test if verify works or not
 router.get('/login/:username/:password', function(req, res){
