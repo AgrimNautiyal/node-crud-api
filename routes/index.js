3d0602724ab3a3e7b340e79002e711ee38d15f77
unknown <agrimnautiyal1999@gmail.com>
2020-07-23 00:40:26 +0530

    Create record experiment with MongoDB was successful

--- routes/index.js
+++ routes/index.js
@@ -6,32 +6,64 @@ require('dotenv').config();
 var uname = process.env.uname;
 var password = process.env.password;
 var db_name = process.env.db_name;
+var users_coll = process.env.users_coll;
 
 const uri = `mongodb+srv://${uname}:${password}@cluster0.ntbt5.mongodb.net/${db_name}?retryWrites=true&w=majority`;
+//connect to mongodb client
+const client = new MongoClient(uri ,{ useNewUrlParser: true, useUnifiedTopology: true });
 
-router.get('/', async function(req, res){
+
+router.get('/', function(req, res){
   console.log("Home route activated");
-  const client = new MongoClient(uri ,{ useNewUrlParser: true, useUnifiedTopology: true });
-  try {
-       // Connect to the MongoDB cluster
-       await client.connect();
-       console.log('connected Successfully');
-       // Make the appropriate DB calls
-       await  listDatabases(client);
-
-   } catch (e) {
-       console.error(e);
-   } finally {
-       await client.close();
-   }
-   res.json({message : 'Successfully connected to mongo'});
+  res.json({message : "HI"});
 });
 
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
+
+
+
+//function to create a record
+async function createListing(client, newListing){
+
+    const result = await client.db(db_name).collection(users_coll).insertOne(newListing);
+    console.log(`New listing created with the following id: ${result.insertedId}`);
+}
+//route to sign up and CREATE a record in the collection
+router.get('/signup/:username/:password', async function(req, res){
+
+    console.log("Signup route is active");
+    var username = req.params.username
+    var password = req.params.password
+
+    var listing = {
+        username : username,
+        password : password
+    }
+
+
+    try{
+      await client.connect();
+      console.log("client connected successfully");
+    await createListing(client, listing);
+
+    res.json({status : "INSERTED"});
+  }
+  catch (e)
+  {
+    console.error(e);
+    res.json({status : "INSERTION FAILED"});
+  }
+  finally
+  {
+    await client.close();
+    res.json({status : "INSERTION TRIAL HAS ENDED"});
+  }
+});
+
+
+
+
+
+
 
 //to test if verify works or not
 router.get('/login/:username/:password', function(req, res){
