418e88ee96e341046b0b7c92c03680b069df12b9
unknown <agrimnautiyal1999@gmail.com>
2020-07-22 21:14:21 +0530

    Request -> Param -> Response experiment

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,22 @@
 var router = require('express').Router();
 var request = require('request');
+
+
 router.get('/', function(req, res){
   console.log("Home route activated");
     res.json({message : 'response'});
 });
+
+
+router.get('/login/:username/:password', function(req, res){
+  console.log("Login Route activated");
+  var username = req.params.username
+  var password = req.params.password
+
+  res.json({username : username,  password:password});
+});
+
+
+
+
 module.exports = router;
